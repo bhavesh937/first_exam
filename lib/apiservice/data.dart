17a6f33318df58058b35a93621f985066e080be2
bhavesh937 <bhaveshdabhi937@gmail.com>
2024-02-16 19:21:13 +0530

    news api
    news api

---
+++ lib/apiservice/data.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../model/models.dart';
+
+class News {
+  List<ArticleModel> news = [];
+  Future<void> getNews() async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=54145bc9681c42de9a6cc831aa90502b";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+            title: element["title"],
+            description: element["description"],
+            url: element["url"],
+            urlToImage: element["urlToImage"],
+            content: element["content"],
+            author: element["author"],
+          );
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
+
+class ShowCategoryNews {
+  List<ShowCategoryModel> categories = [];
+
+  Future<void> getCategoriesNews(String category) async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=us&category=$category&apiKey=54145bc9681c42de9a6cc831aa90502b";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ShowCategoryModel categoryModel = ShowCategoryModel(
+            title: element["title"],
+            description: element["description"],
+            url: element["url"],
+            urlToImage: element["urlToImage"],
+            content: element["content"],
+            author: element["author"],
+          );
+          categories.add(categoryModel);
+        }
+      });
+    }
+  }
+}
+
+class Sliders {
+  List<sliderModel> sliders = [];
+
+  Future<void> getSlider() async {
+    String url =
+        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=8eb940539c874fa98a2050d4afde5d5b";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          sliderModel slidermodel = sliderModel(
+            title: element["title"],
+            description: element["description"],
+            url: element["url"],
+            urlToImage: element["urlToImage"],
+            content: element["content"],
+            author: element["author"],
+          );
+          sliders.add(slidermodel);
+        }
+      });
+    }
+  }
+}
